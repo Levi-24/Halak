3a5b80c702ba47dbc8a8f9cc86cfb50eb5ee02d3
Levente <122294490+Levi-24@users.noreply.github.com>
2024-11-13 13:54:33 +0100

    Add project files.

---
+++ Fish.cs
@@ -0,0 +1,72 @@
+﻿namespace HalakPalinka;
+
+internal class Fish
+{
+    private int top;
+    private int depth;
+    private string species;
+    private double weight;
+    private bool weightIsSet = false;
+
+    public string Species 
+    {
+        get => species;
+        set
+        {
+            if (value is null)
+                throw new Exception("You didnt specify a species.");
+            if(value.Length < 3 || value.Length > 30)throw new Exception(
+                "Species must be between 3 and 30 characters long.");
+            species = value;
+        } 
+    }
+    public bool Predator { get; }
+    public double Weight 
+    { 
+        get => weight; 
+        set
+        {
+            if (value < .5 || value > 50)
+                throw new Exception("Weight can only be between 5 and 40.");
+            if (weightIsSet && (value < weight * .9 || value > weight * 1.1)) throw new Exception(
+                "Weight can only be changed by 10%.");
+            weight = value;
+            weightIsSet = true;
+        }
+    }
+    public int Top
+    {
+        get => depth; set
+        {
+            if (value < 0 || value > 400)
+            {
+                throw new Exception($"Top must be between 0 and 400 cm. And your input is:{value}");
+            }
+            depth = value;
+        }
+    }
+    public int Depth
+    {
+        get => depth; set
+        {
+            if (value < 10 || value > 400)
+            {
+                throw new Exception($"Depth must be between 10 and 400 cm. And your input is:{value}");
+            }
+            depth = value;
+        }
+    }
+    public int Bottom => Top + Depth;
+
+    public override string ToString() =>
+        $"{Species} ({Weight:0.00} kg) - Top: {Top} cm, Depth: {Depth} cm, {(Predator ? "Carnivore" : "Herbivore")}";
+
+    public Fish(string species, bool predator, double weight, int top, int depth)
+    {
+        Species = species;
+        Predator = predator;
+        Weight = weight;
+        Top = top;
+        Depth = depth;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using HalakPalinka;
+
+List<Fish> fishList = new();
+string[] herbFishSpecies = ["Catfish", "Salmon", "Tuna", "Cod", "Shark", "Eel", "Herring", "Trout", "Bass", "Pufferfish"];
+string[] predFishSpecies = ["Bream", "Carp", "Clownfish"];
+
+for (int i = 0; i < 100; i++)
+{
+    bool predator = Random.Shared.Next(100) < 10;
+
+    fishList.Add(new Fish(
+        species: predator ? predFishSpecies[Random.Shared.Next(predFishSpecies.Length)] : herbFishSpecies[Random.Shared.Next(herbFishSpecies.Length)],
+        predator: predator,
+        weight: Random.Shared.Next(5, 401) / 10f,
+        top: Random.Shared.Next(401),
+        depth: Random.Shared.Next(10, 401)));
+
+    Console.ForegroundColor = fishList.Last().Predator ? ConsoleColor.Red : ConsoleColor.Green;
+    Console.WriteLine(fishList.Last());
+}
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine();
+Console.WriteLine("Carnivore fish count:");
+Console.WriteLine("\t" + fishList.Count(x => x.Predator));
+
+Console.WriteLine("Herbivore fish count:");
+Console.WriteLine("\t" + fishList.Count(x => !x.Predator));
+
+Console.WriteLine("Largest fish:");
+Console.WriteLine("\t" + fishList.Max(x => x.Weight));
+
+Console.WriteLine("Fish count on 1.1m:");
+Console.WriteLine("\t" + fishList.Count(x => x.Top < 110 && x.Bottom >= 110));
